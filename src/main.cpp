38dcc28dd36d98070dc00c92cc9c0ef67b52609a
kinwong <kinhangwong@gmail.com>
2021-08-26 03:00:03 +1000

    added a few samples.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <algorithm>
 #include "sources.hpp"
 
 int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
+    //std::cout << "Hello Easy C++ project!" << std::endl;
+    //sources::create_timer();
+    sources::test_combine_latest();
 }
 
--- src/sources.hpp
+++ src/sources.hpp
@@ -1,6 +1,57 @@
-#include <rxcpp/rx.hpp>
+#include "rxcpp/rx.hpp"
+
+namespace rx
+{
+    using namespace rxcpp;
+    using namespace rxcpp::sources;
+    using namespace rxcpp::operators;
+    using namespace rxcpp::util;
+}
 
 namespace sources
 {
-  
+    void test()
+    {
+        auto scheduler = rxcpp::observe_on_one_worker(rxcpp::observe_on_new_thread());
+        auto start = scheduler.now() + std::chrono::milliseconds(10000);
+        auto values = rxcpp::observable<>::timer(start, scheduler);
+        values.as_blocking().subscribe(
+            [](int v)
+            { printf("OnNext: %d\n", v); },
+            []()
+            { printf("OnCompleted\n"); });
+        printf("//! [threaded timepoint timer sample]\n");
+    }
+
+    void create_timer()
+    {
+        auto scheduler = rxcpp::observe_on_new_thread();
+        auto start = scheduler.now() + std::chrono::milliseconds(1000);
+        auto period = std::chrono::milliseconds(250);
+        auto values = rxcpp::observable<>::interval(start, period, scheduler);
+        values.take(30).as_blocking().subscribe(
+            [](int v)
+            { printf("OnNext: %d\n", v); },
+            []()
+            { printf("OnCompleted\n"); });
+
+        printf("//! [threaded interval sample]\n");
+    }
+
+    void test_combine_latest()
+    {
+        printf("//! [combine_latest sample]\n");
+        auto o1 = rxcpp::observable<>::interval(std::chrono::milliseconds(2));
+        auto o2 = rxcpp::observable<>::interval(std::chrono::milliseconds(3));
+        auto o3 = rxcpp::observable<>::interval(std::chrono::milliseconds(5));
+        auto values = o1.combine_latest(o2, o3);
+        values.take(50)
+            .as_blocking()
+            .subscribe(
+            [](std::tuple<int, int, int> v)
+            { printf("OnNext: %d, %d, %d\n", std::get<0>(v), std::get<1>(v), std::get<2>(v)); },
+            []()
+            { printf("OnCompleted\n"); });
+        printf("//! [combine_latest sample]\n");
+    }
 }
